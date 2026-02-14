bbe9ca65506b2950c5bc502db33dfd67429f5046
hffmnnj <52758545+hffmnnj@users.noreply.github.com>
2026-02-14 11:02:10 -0700

    feat(core): add web search provider resolver and factory

--- src/tools/web-search/index.ts
+++ src/tools/web-search/index.ts
@@ -1,4 +1,6 @@
 export * from "./errors";
+export * from "./provider-factory";
 export * from "./provider-contract";
+export * from "./provider-resolver";
 export * from "./tool-contract";
 export * from "./types";
