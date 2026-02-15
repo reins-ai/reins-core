93df0a44cc5d5e28310c784eb7579545d7515d8e
hffmnnj <52758545+hffmnnj@users.noreply.github.com>
2026-02-15 12:46:25 -0700

    feat(core): add channel message router

--- src/channels/index.ts
+++ src/channels/index.ts
@@ -1,6 +1,7 @@
 export * from "./discord";
 export * from "./errors";
 export * from "./registry";
+export * from "./router";
 export * from "./storage";
 export * from "./telegram";
 export * from "./types";
